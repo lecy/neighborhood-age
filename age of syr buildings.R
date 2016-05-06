462c5076f909ba9ed13c90f6f5365511bd796792
Jesse Lecy <jdlecy@gmail.com>
2016-05-05 22:57:01 -0400

    saf

--- age of syr buildings.R
+++ age of syr buildings.R
@@ -1,9 +1,26 @@
 
 
-setwd( "C:/Users/jdlecy/Dropbox/02 - CLASSES/02 - MASTERS/09 - DDM II/SYR Parcels" )
 
 library( maptools )
 library( sp )
+
+
+
+
+dir.create( "syrparcels" )
+
+setwd( "./syrparcels" )
+
+download.file( "https://github.com/lecy/neighborhood-age/raw/master/Data/01-05-2015.zip", "syr_parcels.zip" )
+
+unzip( "syr_parcels.zip" )
+
+file.remove( "syr_parcels.zip" )
+
+
+
+
+
 
 syr <- readShapePoly( fn="01-05-2015", proj4string=CRS("+proj=longlat +datum=WGS84") )
 
@@ -25,11 +42,19 @@ summary( dat$YearBuilt )
 
 dat$YearBuilt[ dat$YearBuilt == 0 ] <- NA
 
+hist( dat$YearBuilt, col="gray", border="white" )
+
+
+
+
 # create color ramp
 
 color.function <- colorRampPalette( c("firebrick4","light gray","darkblue" ) )
 
 col.ramp <- color.function( 7 ) # number of groups you desire
+
+
+
 
 # create the color vector
 
@@ -37,10 +62,14 @@ eras <- c(0,1900,1920,1940,1960,1980,2000,2015)
   
 col.vec <- as.character( cut( dat$YearBuilt, breaks=eras, labels=col.ramp ) )
 
+
+
+
 # plot the choropleth map
 
 par( mar=c(0,0,1,0) )
-# par( mfrow=c(1,1) )
+
+
 plot( syr, col=col.vec, border=NA, main="Age of Buildings" )
 
 legend.text=c("1800-1900","1901-1920","1921-1941","1941-1960","1961-1980","1981-2000","2001-2015")
@@ -52,3 +81,32 @@ legend( "bottomright", bg="white",
         box.col="white",
         title="Period of Construction" 
        )
+       
+
+
+
+
+
+
+       
+
+
+dir.create( "Results" )
+       
+pdf( "./Results/Age of Houses.pdf" )
+
+par( mar=c(0,0,1,0) )
+
+plot( syr, col=col.vec, border=NA, main="Age of Buildings" )
+
+legend.text=c("1800-1900","1901-1920","1921-1941","1941-1960","1961-1980","1981-2000","2001-2015")
+
+legend( "bottomright", bg="white",
+        pch=19, pt.cex=1.5, cex=0.7,
+        legend=legend.text, 
+        col=col.ramp, 
+        box.col="white",
+        title="Period of Construction" 
+       )
+       
+dev.off() 
